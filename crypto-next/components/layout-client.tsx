fb6b7539bc87adcf3aacf70217341a37570fc81a
Complexia <complexia701@gmail.com>
2024-09-20 21:15:07 +0800

    sidebar

--- crypto-next/components/layout-client.tsx
+++ crypto-next/components/layout-client.tsx
@@ -1,1 +1,41 @@
 "use client";
+
+
+import Sidebar from "./ui/sidebar";
+import { usePathname } from 'next/navigation'
+
+
+
+
+const LayoutClient = ({ children }) => {
+
+
+    return (
+
+
+
+
+        <main className="min-h-screen h-screen w-screen flex flex-row ">
+            <Sidebar />
+            
+            <div className="flex flex-row">
+
+
+
+
+
+                <div className="mx-12 my-12">
+                    {children}
+                </div>
+            </div>
+
+
+
+        </main>
+
+
+
+    )
+}
+
+export default LayoutClient;
